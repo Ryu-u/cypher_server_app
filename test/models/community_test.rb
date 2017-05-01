607f1ba6bf48a1ece0f60f098ba2731362df1a39
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-02 02:47:32 +0900

    create test of relation between community and host

--- test/models/community_test.rb
+++ test/models/community_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommunityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'relation between community and host' do
+    hosted_community = Community.new(name:'aaaa',
+                           home:'豊中',
+                           bio:'頑張ります')
+    hosted_community.save
+    hosting_user = User.new(name:'name:aaaa_host')
+    hosting_user.save
+
+    hosted_community.hosts = hosting_user
+    refute_nil hosted_community.hosts, 'failure of relation between community and host'
+  end
 end
