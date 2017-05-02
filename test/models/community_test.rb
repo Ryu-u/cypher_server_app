5695d8b527b0a002ab039aa4d67b6e950d8da8bc
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-02 20:37:43 +0900

    create test

--- test/models/community_test.rb
+++ test/models/community_test.rb
@@ -12,4 +12,17 @@ class CommunityTest < ActiveSupport::TestCase
     hosted_community.hosts << hosting_user
     refute_nil hosted_community.hosts, 'failure of relation between community and host'
   end
+
+  test 'relation between community and participant' do
+    joined_community = Community.new(name:'aaaa',
+                                     home:'豊中',
+                                     bio:'頑張ります')
+    joined_community.save
+    joining_user = User.new(name:'name:aaaa_participant')
+    joining_user.save
+
+    joined_community.participants << joining_user
+    refute_nil joined_community.participants, 'failure of relation between community and participant'
+  end
+  
 end
