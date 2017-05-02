3d381fdf79f9ec7293f353eb7c11eb0c86345669
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-03 01:52:07 +0900

    create test

--- test/models/cypher_test.rb
+++ test/models/cypher_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CypherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'relation between community and cypher' do
+    community = Community.create(name:'aaaa-community')
+    cypher = Cypher.create(name:'aaaa-cypher')
+
+    cypher.community = community
+
+    refute_nil community.cyphers, 'failure of relation between community and cyphers'
+  end
 end
