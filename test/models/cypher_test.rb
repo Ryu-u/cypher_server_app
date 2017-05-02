201fda402d67cdd03f2332c6a25bd35f27cf3b5c
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-03 02:52:41 +0900

    create test

--- test/models/cypher_test.rb
+++ test/models/cypher_test.rb
@@ -10,4 +10,16 @@ class CypherTest < ActiveSupport::TestCase
     community.cyphers << cypher
     refute_nil community.cyphers, 'failure of relation between community and cyphers'
   end
+
+  test 'relation between cypher and host' do
+    community = Community.create(name:'aaaa-community')
+    cypher = Cypher.create(name:'aaaa-cypher')
+
+    cypher.community = community
+
+    host = User.create(name:'aaaa-host')
+    community.hosts << host
+    cypher.host = host
+    refute_nil cypher.host, 'failure of relation between cypher and host'
+  end
 end
