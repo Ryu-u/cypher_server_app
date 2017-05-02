4a64853a3fc7a106240238210bdf9ae4d49c3169
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-02 20:33:40 +0900

    migrate communityparticipants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501181011) do
+ActiveRecord::Schema.define(version: 20170502113209) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170501181011) do
     t.integer  "host_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "community_participants", force: :cascade do |t|
+    t.integer  "community_id"
+    t.integer  "participant_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
