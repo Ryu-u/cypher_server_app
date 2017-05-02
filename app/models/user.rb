5518c3122c9407503aa9eebc1b234676d012b5c9
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-02 20:43:29 +0900

    declare user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :community_hosts, foreign_key: 'host_id'
   has_many :communities, through: :community_hosts
+
+  has_many :community_participants, foreign_key: 'participant_id'
+  has_many :communities, through: :community_participants
 end
