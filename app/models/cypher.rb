eaeedb8ffe196dc0ac137e074a35658dd8ac344f
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-03 04:13:04 +0900

    declare cypher association

--- app/models/cypher.rb
+++ app/models/cypher.rb
@@ -1,4 +1,7 @@
 class Cypher < ApplicationRecord
   belongs_to :community
   belongs_to :host, class_name: 'User', foreign_key: 'host_id'
+
+  has_many :cypher_participants
+  has_many :participants, class_name: 'User', through: :cypher_participants
 end
