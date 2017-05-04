8e0e772870db9b9d18716b8d6edf96f2009be782
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 04:41:41 +0900

    migrate community_tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504173036) do
+ActiveRecord::Schema.define(version: 20170504180109) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20170504173036) do
     t.integer  "participant_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "community_tags", force: :cascade do |t|
+    t.integer  "community_id"
+    t.integer  "tag_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "cypher_participants", force: :cascade do |t|
