17ad8dbf7d653c2ff14bf6aed7f43e75c9d1d62e
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 02:31:36 +0900

    migrate tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504164115) do
+ActiveRecord::Schema.define(version: 20170504173036) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name"
@@ -83,6 +83,12 @@ ActiveRecord::Schema.define(version: 20170504164115) do
     t.string   "place"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
