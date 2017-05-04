5c58083905eef7c02136ec54b88aaf01707d4e53
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 04:42:26 +0900

    beforeブロックにletが書けなかったのでインスタンス変数使用へ変更

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 describe Tag do
   before do
-    let(:community){create(:community)}
-    let(:user){create(:user)}
-    let(cypher){build(:cypher)}
-    cypher.host = user
-    cypher.community = community
-    cypher.save
+    @community = create(:community)
+    @user = create(:user)
+    @cypher = build(:cypher)
 
-    let(:tag){build(:tag)}
-    community.tags << tag
-    tag.save
+    @cypher.community = @community
+    @cypher.host = @user
+    @cypher.save
+    @tag = build(:tag)
+
+    @community.tags << @tag
+    @tag.save
   end
-  
-  context 'relation to community' do
-    it 'belongs to community' do
-      expect(community.tags).to eq tag.all
+
+  describe 'on community' do
+    it 'has many communities' do
+      expect(@tag.communities.first).to eq @community
+
     end
+
   end
+
+
 
 end
