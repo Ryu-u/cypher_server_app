a2485bc69ca27d065d5b6efc4ceed620365ced5e
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 02:58:11 +0900

    create test on relation to community

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tag do
+  before do
+    let(:community){create(:community)}
+    let(:user){create(:user)}
+    let(cypher){build(:cypher)}
+    cypher.host = user
+    cypher.community = community
+    cypher.save
+
+    let(:tag){build(:tag)}
+    community.tags << tag
+    tag.save
+  end
+  
+  context 'relation to community' do
+    it 'belongs to community' do
+      expect(community.tags).to eq tag.all
+    end
+  end
+
 end
