970aeace254571d574e0dc79c6af719b285816ea
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 01:28:59 +0900

    migrate regular_cypher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504083522) do
+ActiveRecord::Schema.define(version: 20170504162205) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name"
@@ -72,6 +72,17 @@ ActiveRecord::Schema.define(version: 20170504083522) do
     t.string   "directory_url"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "regular_cyphers", force: :cascade do |t|
+    t.string   "community_id"
+    t.text     "info"
+    t.integer  "cypher_day"
+    t.string   "cypher_from"
+    t.string   "cypher_to"
+    t.string   "place"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
