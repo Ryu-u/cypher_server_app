a7632d607fecfb3106024b2c538132fe354ba947
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-05 04:49:32 +0900

    add test on relation between cypher and tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -12,17 +12,19 @@ describe Tag do
     @tag = build(:tag)
 
     @community.tags << @tag
+    @cypher.tags << @tag
     @tag.save
   end
 
   describe 'on community' do
     it 'has many communities' do
       expect(@tag.communities.first).to eq @community
-
     end
-
   end
-
+  describe 'on cypher' do
+    it 'has many cyphers' do
+      expect(@tag.cyphers.first).to eq @cypher
+    end
 
 
 end
