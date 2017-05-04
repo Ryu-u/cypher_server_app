2952f635810c47749e842ef6681eded16ce2bdce
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-04 17:36:22 +0900

    modify application.rb because of model generating error

--- config/application.rb
+++ config/application.rb
@@ -13,14 +13,4 @@ module CypherServer
     # -- all .rb files in that directory are automatically loaded.
   end
 
-  config.generators do |g|
-    g.test_framework :rspec,
-                     fixtures: true,
-                     view_specs: false,
-                     helper_specs: false,
-                     routing_specs: false,
-                     controller_specs: true,
-                     request_specs: false
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
-  end
 end
