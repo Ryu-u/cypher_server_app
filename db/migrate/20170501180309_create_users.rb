9b6ede33bb8f6a8dd4e64982f0bf88361cc53c53
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-06 21:24:51 +0900

    add not null constraint to user model

--- db/migrate/20170501180309_create_users.rb
+++ db/migrate/20170501180309_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :home
-      t.text :bio
-      t.integer :type_flag
+      t.string :name, null: false
+      t.string :home, null: false
+      t.text :bio, null: false
+      t.integer :type_flag, null: false
       t.string :twitter_account
       t.string :facebook_account
       t.string :thumbnail_url
