860af633a208584154752f6e5ec98043b9dba3f9
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-06 14:58:22 +0900

    add not null constraints to community

--- db/migrate/20170501173837_create_communities.rb
+++ db/migrate/20170501173837_create_communities.rb
@@ -1,9 +1,9 @@
 class CreateCommunities < ActiveRecord::Migration[5.0]
   def change
     create_table :communities do |t|
-      t.string :name
-      t.string :home
-      t.text :bio
+      t.string :name, null: false
+      t.string :home, null: false
+      t.text :bio, null: false
       t.string :twitter_account
       t.string :facebook_account
       t.string :thumbnail_url
