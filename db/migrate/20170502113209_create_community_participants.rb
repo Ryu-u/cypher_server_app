1b82f7096ba00f43c3a3adc8addb874c0a0e549a
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-09 07:30:06 +0900

    add not null constraint to communityparticipant model

--- db/migrate/20170502113209_create_community_participants.rb
+++ db/migrate/20170502113209_create_community_participants.rb
@@ -1,8 +1,8 @@
 class CreateCommunityParticipants < ActiveRecord::Migration[5.0]
   def change
     create_table :community_participants do |t|
-      t.integer :community_id
-      t.integer :participant_id
+      t.integer :community_id, null: false
+      t.integer :participant_id, null: false
 
       t.timestamps
     end
