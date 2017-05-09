b7c48b87ad0407f92049f75ec5b41a50f1ecad22
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:09:34 +0900

    add not null constraint to posts

--- db/migrate/20170504083522_create_posts.rb
+++ db/migrate/20170504083522_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.integer :user_id
-      t.integer :cypher_id
+      t.integer :user_id, null: false
+      t.integer :cypher_id, null: false
       t.string :directory_url
 
       t.timestamps
