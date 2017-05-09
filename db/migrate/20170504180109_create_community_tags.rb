d92cba1ae65b788c3bdd8e8141055326a86a75e3
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:30:15 +0900

    add not null constraint to community_tags

--- db/migrate/20170504180109_create_community_tags.rb
+++ db/migrate/20170504180109_create_community_tags.rb
@@ -1,8 +1,8 @@
 class CreateCommunityTags < ActiveRecord::Migration[5.0]
   def change
     create_table :community_tags do |t|
-      t.integer :community_id
-      t.integer :tag_id
+      t.integer :community_id, null: false
+      t.integer :tag_id, null: false
 
       t.timestamps
     end
