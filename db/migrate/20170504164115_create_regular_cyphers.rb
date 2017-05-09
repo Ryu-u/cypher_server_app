be483f8b1af36252822934c2ad193ea2389fa691
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:18:05 +0900

    add not null constraint to regular_cypher

--- db/migrate/20170504164115_create_regular_cyphers.rb
+++ db/migrate/20170504164115_create_regular_cyphers.rb
@@ -1,12 +1,12 @@
 class CreateRegularCyphers < ActiveRecord::Migration[5.0]
   def change
     create_table :regular_cyphers do |t|
-      t.integer :community_id
-      t.text :info
-      t.integer :cypher_day
-      t.string :cypher_from
-      t.string :cypher_to
-      t.string :place
+      t.integer :community_id, null: false
+      t.text :info, null: false
+      t.integer :cypher_day, null: false
+      t.string :cypher_from, null: false
+      t.string :cypher_to, null: false
+      t.string :place, null: false
 
       t.timestamps
     end
