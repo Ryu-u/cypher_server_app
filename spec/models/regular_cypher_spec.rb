d85a4993d3c295009beabc3037815019b13f58e1
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:16:21 +0900

    create regular_cypher test

--- spec/models/regular_cypher_spec.rb
+++ spec/models/regular_cypher_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 describe RegularCypher do
-  it 'belongs to community' do
-    community = create(:community)
-    regular_cypher = build(:regular_cypher)
-    community.regular_cypher = regular_cypher
+  context 'add not null constraint' do
+    it {have_not_null_constraint_on(:community_id)}
+    it {have_not_null_constraint_on(:info)}
+    it {have_not_null_constraint_on(:cypher_day)}
+    it {have_not_null_constraint_on(:cypher_from)}
+    it {have_not_null_constraint_on(:cypher_to)}
+    it {have_not_null_constraint_on(:place)}
+  end
 
-    regular_cypher.save
-    expect(regular_cypher.community_id).to eq community.id
+  context 'association' do
+    it 'belongs to community' do
+      community = create(:community)
+      regular_cypher = build(:regular_cypher)
+      community.regular_cypher = regular_cypher
+
+      regular_cypher.save
+      expect(regular_cypher.community_id).to eq community.id
+    end
   end
 end
