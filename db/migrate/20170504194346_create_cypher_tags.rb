2a78d33a8f26c7cbda33a2f7d0f66abe415c11cc
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:33:43 +0900

    add not null constraint to cypher_tags

--- db/migrate/20170504194346_create_cypher_tags.rb
+++ db/migrate/20170504194346_create_cypher_tags.rb
@@ -1,8 +1,8 @@
 class CreateCypherTags < ActiveRecord::Migration[5.0]
   def change
     create_table :cypher_tags do |t|
-      t.integer :cypher_id
-      t.integer :tag_id
+      t.integer :cypher_id, null: false
+      t.integer :tag_id, null: false
 
       t.timestamps
     end
