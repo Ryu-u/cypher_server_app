d38c704faf4504aa7e8cab7b7935e8414f458aef
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 04:49:22 +0900

    add not null constraint to community_follower table

--- db/migrate/20170502115658_create_community_followers.rb
+++ db/migrate/20170502115658_create_community_followers.rb
@@ -1,8 +1,8 @@
 class CreateCommunityFollowers < ActiveRecord::Migration[5.0]
   def change
     create_table :community_followers do |t|
-      t.integer :community_id
-      t.integer :follower_id
+      t.integer :community_id, null: false
+      t.integer :follower_id, null: false
 
       t.timestamps
     end
