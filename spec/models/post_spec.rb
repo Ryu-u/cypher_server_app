5e4a11fc37b996e17bf6b5af8bc6350e98651f1e
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:07:55 +0900

    create post test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
 describe Post do
-  it "belongs to cypher and user" do
-    community = create(:community)
-    user = create(:user)
+  context 'add not null constraint' do
+    it {have_not_null_constraint_on(:user_id)}
+    it {have_not_null_constraint_on(:cypher_id)}
+  end
 
-    cypher = build(:cypher)
+  content 'association' do
+    it "belongs to cypher and user" do
+      community = create(:community)
+      user = create(:user)
 
-    cypher.community = community
-    cypher.host = user
+      cypher = build(:cypher)
 
-    cypher.save
+      cypher.community = community
+      cypher.host = user
 
-    post = build(:post)
+      cypher.save
 
-    post.cypher = cypher
-    post.user = user
+      post = build(:post)
 
-    post.save
+      post.cypher = cypher
+      post.user = user
 
-    expect(post.cypher_id).to eq cypher.id
-    expect(post.user_id).to eq user.id
+      post.save
+
+      expect(post.cypher_id).to eq cypher.id
+      expect(post.user_id).to eq user.id
+    end
   end
 end
