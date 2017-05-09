eb4755789c10881d27a4b7f7068adac6d8c61286
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-10 05:01:39 +0900

    add not null constraint to cypher_participants

--- db/migrate/20170502184414_create_cypher_participants.rb
+++ db/migrate/20170502184414_create_cypher_participants.rb
@@ -1,8 +1,8 @@
 class CreateCypherParticipants < ActiveRecord::Migration[5.0]
   def change
     create_table :cypher_participants do |t|
-      t.integer :cypher_id
-      t.integer :participant_id
+      t.integer :cypher_id, null: false
+      t.integer :participant_id, null: false
 
       t.timestamps
     end
