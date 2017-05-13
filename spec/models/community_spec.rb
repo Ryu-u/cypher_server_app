7b39bbcb014d10fdf1126e4cc418b846d21e7516
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-13 17:21:53 +0900

    beforeは不要だったので削除&validate_uniqueness_ofの仕様のためにsubject咥え、別途describeブロックを追加

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -2,23 +2,19 @@ require 'rails_helper'
 
 describe Community do
   describe 'with DB constraint' do
-    before do
-      @community = create(:community)
-    end
-
     it {have_not_null_constraint_on(:name)}
-
     it {have_not_null_constraint_on(:home)}
-
     it {have_not_null_constraint_on(:bio)}
   end
 
   describe 'with validation' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validates_uniqueness_of(:name)}
-
     it { is_expected.to validate_presence_of(:home) }
-
     it { is_expected.to validate_presence_of(:bio) }
+
+    describe 'of uniqueness' do
+      subject{build(:community)}
+      it { is_expected.to validate_uniqueness_of(:name) }
+    end
   end
 end
