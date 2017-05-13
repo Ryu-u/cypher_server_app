37cc879739ae416b320978a57272208c889753e5
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-13 18:17:50 +0900

    Communitiesテーブルのnameカラム用のユニークテストを作成

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -5,6 +5,17 @@ describe Community do
     it {have_not_null_constraint_on(:name)}
     it {have_not_null_constraint_on(:home)}
     it {have_not_null_constraint_on(:bio)}
+
+    describe 'of uniqueness' do
+      it 'should not have the same value in different records of name column ' do
+        expect do
+          existing_community = Community.create(name:"AAAA", home:"AAAA", bio:"AAAA")
+          new_community = build(:community)
+          new_community.name = existing_community.name
+          new_community.save!(validate: false)
+        end.to raise_error( ActiveRecord::RecordNotUnique )
+      end
+    end
   end
 
   describe 'with validation' do
