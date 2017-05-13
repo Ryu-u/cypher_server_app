6426da1d71b883a48c7ee01cfa6abf70edf0d111
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-13 18:21:54 +0900

    migrate community

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513083125) do
+ActiveRecord::Schema.define(version: 20170504194346) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name",             null: false
