eb5709ae5be7fc6208e1e8e808ca3924ad8fc045
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 21:25:06 +0900

    create post test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,32 +1,54 @@
 require 'rails_helper'
 
 describe Post do
-  context 'add not null constraint' do
-    it {have_not_null_constraint_on(:user_id)}
-    it {have_not_null_constraint_on(:cypher_id)}
+  describe 'with DB' do
+      it {have_not_null_constraint_on(:user_id)}
+      it {have_not_null_constraint_on(:cypher_id)}
+
+    describe 'of index' do
+      it 'should not have two records whith have the same combination of cypher_id and user_id' do
+        expect do
+          community = Community.create(name: "aaaa", home: "aaaa", bio: "aaaa")
+          host = User.create(name: "aaaa", home: "aaaa", bio: "aaaa", type_flag:1)
+          community.hosts << host
+          community.save
+          cypher = Cypher.new(name: "aaaa",
+                              serial_num:1,
+                              info:"aaaa",
+                              cypher_from:DateTime.now(),
+                              cypher_to:DateTime.now(),
+                              place:"aaaa")
+          cypher.community = community
+          cypher.host = host
+          cypher.save
+
+          post = Post.new
+          poster = User.create(name: "bbbb", home: "bbbb", bio: "bbbb", type_flag:1)
+          post.cypher = cypher
+          post.user = poster
+          post.save
+
+          another_post = post.new(cypher_id: cypher.id, user_id: user.id)
+          another_post.save!(validate: false)
+        end.to raise_error(ActiveRecord::RecordNotUnique)
+      end
+    end
   end
 
-  context 'association' do
-    it "belongs to cypher and user" do
-      community = create(:community)
-      user = create(:user)
+  describe 'with model' do
+    describe 'association' do
+      it {is_expected.to belong_to(:cypher)}
+      it {is_expected.to belong_to(:user)}
+    end
 
-      cypher = build(:cypher)
+    describe 'validation' do
+      it { is_expected.to validate_presence_of(:cypher_id) }
+      it { is_expected.to validate_presence_of(:user_id) }
 
-      cypher.community = community
-      cypher.host = user
-
-      cypher.save
-
-      post = build(:post)
-
-      post.cypher = cypher
-      post.user = user
-
-      post.save
-
-      expect(post.cypher_id).to eq cypher.id
-      expect(post.user_id).to eq user.id
+      describe 'of uniqueness' do
+        subject{Post.new(cypher_id:1, user_id:1)}
+        it { is_expected.to validate_uniqueness_of(:cypher_id).scoped_to(:user_id) }
+      end
     end
   end
 end
