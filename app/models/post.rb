e2c96f2eeb6eab0df8ffcc1fefc450ee6c584b7b
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 21:25:26 +0900

    add validation to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :cypher
+
+  validates :cypher_id, presence: true, uniqueness: { scope: :user_id }
+  validates :user_id, presence: true
+
 end
