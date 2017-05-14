14d801d0a52ee00d08f48eb1a3b592025d4442d7
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 19:06:49 +0900

    add validation to CommunityParticipant

--- app/models/community_participant.rb
+++ app/models/community_participant.rb
@@ -1,4 +1,7 @@
 class CommunityParticipant < ApplicationRecord
   belongs_to :community
   belongs_to :participant, foreign_key: 'participant_id', class_name: 'User'
+
+  validates :community_id, presence: true, uniqueness: { scope: :participant_id }
+  validates :participant_id, presence: true
 end
