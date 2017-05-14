6d859a664500201f6485127639063e0a070bec06
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 19:51:43 +0900

    add validation to CypherParticipant

--- app/models/cypher_participant.rb
+++ app/models/cypher_participant.rb
@@ -1,4 +1,7 @@
 class CypherParticipant < ApplicationRecord
   belongs_to :cypher
   belongs_to :participant, class_name: 'User', foreign_key: 'participant_id'
+
+  validates :cypher_id, presence: true, uniqueness:{scope: :participant_id}
+  validates :participant_id, presence: true
 end
