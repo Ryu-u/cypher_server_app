7696e9d37914534e5d586a371af3e3cc56c66766
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 17:01:30 +0900

    add validation to CommunityHost

--- app/models/community_host.rb
+++ app/models/community_host.rb
@@ -1,4 +1,7 @@
 class CommunityHost < ApplicationRecord
   belongs_to :community
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
+
+  validates :community_id, presence: true, uniqueness: { scope: [:host_id] }
+  validates :host_id, presence: true
 end
