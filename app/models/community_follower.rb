a9dc56ff40da80db5941a918e84d0e209493a75a
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-14 19:13:11 +0900

    add validation to CommunityFollower

--- app/models/community_follower.rb
+++ app/models/community_follower.rb
@@ -1,4 +1,7 @@
 class CommunityFollower < ApplicationRecord
   belongs_to :community
   belongs_to :follower, foreign_key: 'follower_id', class_name: 'User'
+
+  validates :community_id, presence: true, uniqueness: { scope: :follower_id }
+  validates :follower_id, presence: true
 end
