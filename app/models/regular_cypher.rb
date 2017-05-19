042fc91311d086a7049f4e3c9529610af396795f
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 08:53:02 +0900

    add validation to RegularCyphers

--- app/models/regular_cypher.rb
+++ app/models/regular_cypher.rb
@@ -1,3 +1,10 @@
 class RegularCypher < ApplicationRecord
   belongs_to :community
+
+  validates :community_id, presence: true, uniqueness: true
+  validates :info, presence: true
+  validates :cypher_day, presence: true
+  validates :cypher_from, presence: true
+  validates :cypher_to, presence: true
+  validates :place, presence: true
 end
