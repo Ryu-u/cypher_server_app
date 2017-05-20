a591524196e1e1256f70a983bc7500ec98bd5750
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 10:36:26 +0900

    カラム名付け忘れ

--- app/models/community_tag.rb
+++ app/models/community_tag.rb
@@ -2,6 +2,6 @@ class CommunityTag < ApplicationRecord
   belongs_to :community
   belongs_to :tag
 
-  validates presence: true, uniqueness: {scope: :tag_id}
-  validates presence: true
+  validates :community_id, presence: true, uniqueness: {scope: :tag_id}
+  validates :tag_id, presence: true
 end
