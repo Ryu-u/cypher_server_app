de6f4bba49ef83078b96013f892c9e27bce01712
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 16:49:01 +0900

    modify CypherParticipant test

--- spec/models/cypher_participant_spec.rb
+++ spec/models/cypher_participant_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe CypherParticipant do
   describe 'with DB' do
-    it {have_not_null_constraint_on(:cypher_id)}
-    it {have_not_null_constraint_on(:participant_id)}
+    context 'not null constraint' do
+      it {have_not_null_constraint_on(:cypher_id)}
+      it {have_not_null_constraint_on(:participant_id)}
+    end
 
-    describe 'of index' do
+    context 'of index' do
       it 'should not have two records whith have the same combination of cypher_id and participant_id' do
         expect do
           community = Community.create(name: "aaaa", home: "aaaa", bio: "aaaa")
@@ -22,24 +24,37 @@ describe CypherParticipant do
           cypher.host = host
           cypher.save
 
-          participant = User.create(name: "bbbb", home: "bbbb", bio: "bbbb", type_flag:1)
+          participant = User.create(name: "bbbb",
+                                    home: "bbbb",
+                                    bio: "bbbb",
+                                    type_flag:1)
           cypher.participants << participant
           cypher.save
 
-          cypher_participant = CypherParticipant.new(cypher_id: cypher.id, participant_id: participant.id)
+          cypher_participant = CypherParticipant.new(cypher_id: cypher.id,
+                                                     participant_id: participant.id)
           cypher_participant.save!(validate: false)
         end.to raise_error(ActiveRecord::RecordNotUnique)
       end
     end
   end
 
-  describe 'with validation' do
-    it { is_expected.to validate_presence_of(:cypher_id) }
-    it { is_expected.to validate_presence_of(:participant_id) }
+  describe 'with model' do
+    context 'validation' do
+      describe 'of presence' do
+        it { is_expected.to validate_presence_of(:cypher_id) }
+        it { is_expected.to validate_presence_of(:participant_id) }
+      end
 
-    describe 'of uniqueness' do
-      subject{CypherParticipant.new(cypher_id:1, participant_id:1)}
-      it { is_expected.to validate_uniqueness_of(:cypher_id).scoped_to(:participant_id) }
+      describe 'of uniqueness' do
+        subject{CypherParticipant.new(cypher_id:1, participant_id:1)}
+        it { is_expected.to validate_uniqueness_of(:cypher_id).scoped_to(:participant_id) }
+      end
+    end
+
+    context 'association' do
+      it{is_expected.to belong_to(:cypher)}
+      it{is_expected.to belong_to(:participant)}
     end
   end
 
