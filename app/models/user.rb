2a277ea414c10a6bc8302b6e4988c51093d1c7be
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 16:00:05 +0900

    User→Communityへの関連名が全て「communities」だとhost,participant,followerのどの関係か特定できないため、userの関連を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   has_many :community_hosts, foreign_key: 'host_id'
-  has_many :communities, through: :community_hosts
+  has_many :hosting_communities,
+            class_name: 'Community',
+            through: :community_hosts
 
   has_many :community_participants, foreign_key: 'participant_id'
-  has_many :communities, through: :community_participants
+  has_many :participating_communities,
+            class_name: 'Community',
+            through: :community_participants
 
   has_many :community_followers, foreign_key: 'follower_id'
-  has_many :communities, through: :community_followers
+  has_many :following_communities,
+            class_name: 'Community',
+            through: :community_followers
 
   has_many :cyphers, class_name: 'Cypher', foreign_key: 'host_id'
 
