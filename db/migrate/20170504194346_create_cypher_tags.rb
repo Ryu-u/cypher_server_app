4b326f406467d8968a5c51a6dac4b89de251cd5b
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 11:13:01 +0900

    add index to cypher_tags

--- db/migrate/20170504194346_create_cypher_tags.rb
+++ db/migrate/20170504194346_create_cypher_tags.rb
@@ -6,5 +6,7 @@ class CreateCypherTags < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+
+    add_index :cypher_tags, [:cypher_id, :tag_id], unique: true, name: 'cyphertags_unique_index'
   end
 end
