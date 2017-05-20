192ee4cc616ab9428517234c4710b1ba8fd50135
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 09:24:26 +0900

    create tag test

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,37 +1,27 @@
 require 'rails_helper'
 
 describe Tag do
-  before do
-    @community = create(:community)
-    @user = create(:user)
-    @cypher = build(:cypher)
-
-    @cypher.community = @community
-    @cypher.host = @user
-    @cypher.save
-    @tag = build(:tag)
-
-    @community.tags << @tag
-    @cypher.tags << @tag
-    @tag.save
-  end
-
-  context 'not null constraint' do
-    it {have_not_null_constraint_on(:content)}
-  end
-
-  context 'association' do
-    describe 'on community' do
-      it 'has many communities' do
-        expect(@tag.communities.first).to eq @community
-      end
-    end
-    describe 'on cypher' do
-      it 'has many cyphers' do
-        expect(@tag.cyphers.first).to eq @cypher
-      end
+  describe 'with DB' do
+    describe 'not null constraint' do
+      it {have_not_null_constraint_on(:content)}
     end
   end
 
+  describe 'with model' do
+    describe 'validation' do
+      context 'of presence' do
+      it {is_expected.to validate_presence_of(:content)}
+      end
 
+      context 'of uniqueness' do
+        subject{build(:tag)}
+        it {is_expected.to validate_uniqueness_of(:content)}
+      end
+
+      context 'association' do
+        it {is_expected.to have_many(:community_tags)}
+        it {is_expected.to have_many(:communities).through(:community_tags)}
+      end
+    end
+  end
 end
