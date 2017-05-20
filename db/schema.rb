440c1828add94ba41b8651cb3dc424fb7971a8eb
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 16:39:23 +0900

    remigrate posts

--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 20170504194346) do
     t.string   "directory_url"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["cypher_id", "user_id"], name: "posts_unique_index", unique: true
   end
 
   create_table "regular_cyphers", force: :cascade do |t|
