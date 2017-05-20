75399a2a77df6132b7701385c131d66de28d62d4
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 15:42:54 +0900

    modify CommunityFollower test

--- spec/models/community_follower_spec.rb
+++ spec/models/community_follower_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe CommunityFollower do
   describe 'with DB' do
-    it {have_not_null_constraint_on(:community_id)}
-    it {have_not_null_constraint_on(:follower_id)}
+    context 'not null constraint' do
+      it {have_not_null_constraint_on(:community_id)}
+      it {have_not_null_constraint_on(:follower_id)}
+    end
 
-    describe 'of index' do
+    context 'of index' do
       it 'should not have two records whith have the same combination of community_id and follower id' do
         expect do
           community = Community.create(name: "aaaa", home: "aaaa", bio: "aaaa")
@@ -19,13 +21,20 @@ describe CommunityFollower do
     end
   end
 
-  describe 'with validation' do
-    it { is_expected.to validate_presence_of(:community_id) }
-    it { is_expected.to validate_presence_of(:follower_id) }
+  describe 'with model' do
+    context 'of presence' do
+      it { is_expected.to validate_presence_of(:community_id) }
+      it { is_expected.to validate_presence_of(:follower_id) }
+    end
 
-    describe 'of uniqueness' do
+    context 'of uniqueness' do
       subject{CommunityFollower.new(community_id:1, follower_id:1)}
       it { is_expected.to validate_uniqueness_of(:community_id).scoped_to(:follower_id) }
+    end
+
+    context 'association' do
+      it {is_expected.to belong_to(:community)}
+      it {is_expected.to belong_to(:follower)}
     end
   end
 end
