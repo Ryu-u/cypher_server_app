b2204d5f1574f372be797fa95b7092ca7a5a0461
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 11:12:43 +0900

    add validation to CypherTag

--- app/models/cypher_tag.rb
+++ app/models/cypher_tag.rb
@@ -1,4 +1,7 @@
 class CypherTag < ApplicationRecord
   belongs_to :cypher
   belongs_to :tag
+
+  validates :cypher_id, presence: true, uniqueness: {scope: :tag_id}
+  validates :tag_id, presence: true
 end
