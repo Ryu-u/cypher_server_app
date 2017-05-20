61cee1de07b206ddd25b5403ace5a9fd7b481bf1
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-20 15:40:20 +0900

    modify CommunityParticipant test

--- spec/models/community_participant_spec.rb
+++ spec/models/community_participant_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe CommunityParticipant do
   describe 'with DB' do
-    it {have_not_null_constraint_on(:community_id)}
-    it {have_not_null_constraint_on(:participant_id)}
+    context 'not null constraint' do
+      it {have_not_null_constraint_on(:community_id)}
+      it {have_not_null_constraint_on(:participant_id)}
+    end
 
-    describe 'of index' do
+    context 'of index' do
       it 'should not have two records whith have the same combination of community_id and participant id' do
         expect do
           community = Community.create(name: "aaaa", home: "aaaa", bio: "aaaa")
@@ -19,13 +21,20 @@ describe CommunityParticipant do
     end
   end
 
-  describe 'with validation' do
-    it { is_expected.to validate_presence_of(:community_id) }
-    it { is_expected.to validate_presence_of(:participant_id) }
+  describe 'with model' do
+    context 'of presence' do
+      it { is_expected.to validate_presence_of(:community_id) }
+      it { is_expected.to validate_presence_of(:participant_id) }
+    end
 
-    describe 'of uniqueness' do
+    context 'of uniqueness' do
       subject{CommunityParticipant.new(community_id:1, participant_id:1)}
       it { is_expected.to validate_uniqueness_of(:community_id).scoped_to(:participant_id) }
+    end
+
+    context 'association' do
+      it {is_expected.to belong_to(:community)}
+      it {is_expected.to belong_to(:participant)}
     end
   end
 end
