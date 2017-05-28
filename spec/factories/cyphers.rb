2c4a016d9d133bbf777fc20bb3ba79fe2bfc6140
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-28 21:26:30 +0900

    use fakers and add factories

--- spec/factories/cyphers.rb
+++ spec/factories/cyphers.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
-  factory :cypher do
-    name "aaaa"
+  factory :cypher, aliases:[:participating_cyphers,
+                           :hosting_cyphers] do
+
+    community
+    host
+
+    name {Faker::Team.name}
     serial_num 1
-    community_id 1
-    info "頑張ります"
+    info {Faker::ChuckNorris.fact}
     cypher_from Date.today.to_datetime
     cypher_to Date.today.to_datetime
     place "豊中"
