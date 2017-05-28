4cc352e3d26a2b4447f0bc946cba95216f175a60
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-28 21:26:09 +0900

    use tods and fakers

--- spec/factories/regular_cyphers.rb
+++ spec/factories/regular_cyphers.rb
@@ -1,10 +1,13 @@
+require 'Faker'
+require 'tod'
 FactoryGirl.define do
   factory :regular_cypher do
-    community_id 1
-    info "MyText"
+    community
+
+    info {Faker::ChuckNorris.fact}
     cypher_day 1
-    cypher_from "MyString"
-    cypher_to "MyString"
-    place "MyString"
+    cypher_from Tod::TimeOfDay.parse("18").to_s
+    cypher_to Tod::TimeOfDay.parse("21").to_s
+    place {Faker::Address.city}
   end
 end
