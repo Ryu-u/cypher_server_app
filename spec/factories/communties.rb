bdbc4cb473eaddc39d8aa6598104a07e6e80e05d
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-05-28 21:25:18 +0900

    add traits and fakers

--- spec/factories/communties.rb
+++ spec/factories/communties.rb
@@ -1,10 +1,72 @@
+require 'Faker'
 FactoryGirl.define do
-  factory :community do
-    name "aaaa"
-    home "豊中"
-    bio "頑張ります"
-    facebook_account "facebook_account"
-    twitter_account "aaaa_twitter"
-    thumbnail_url "aaaa"
+  factory :community, aliases:[:hosting_communities,
+                              :participating_communities,
+                              :following_communities] do
+
+    name {Faker::Team.name}
+    home {Faker::Address.city}
+    bio {Faker::ChuckNorris.fact}
+    facebook_account {"#{name}_fb".downcase}
+    twitter_account {"#{name}_tw".downcase}
+    thumbnail_url {Faker::File.file_name('path/to/',
+                                        "#{name}" + '_fb', 'jpg')}
+
+    trait :with_host do
+      after(:create) do |community|
+        create(:community_host,
+               community: community,
+               host: create(:host))
+      end
+    end
+
+    trait :with_participant do
+      after(:create) do |community|
+        3.times do
+          create(:community_participant,
+                 community: community,
+                 participant: create(:participant))
+        end
+      end
+    end
+
+    trait :with_follower do
+      after(:create) do |community|
+        3.times do
+          create(:community_follower,
+                 community: community,
+                 follower: create(:follower))
+        end
+      end
+    end
+
+    trait :with_cypher do
+      after(:create) do |community|
+        for temp in -2..2 do
+          create(:cypher,
+                 community: community,
+                 host: create(:host),
+                 cypher_from: Date.today.to_datetime + temp,
+                 cypher_to: Date.today.to_datetime + temp + Rational(2,24))
+        end
+      end
+    end
+
+    trait :with_regular_cypher do
+      after(:create) do |community|
+        create(:regular_cypher, community: community)
+      end
+    end
+
+    trait :with_tag do
+      after(:create) do |community|
+        3.times do
+          create(:community_tag,
+                 community: community,
+                 tag: create(:tag))
+        end
+      end
+    end
+
   end
 end
