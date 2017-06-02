0dc97d5c293bde6709486333c67ac8ee5a4a8f10
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-06-03 02:16:46 +0900

    add error hundling

--- app/apis/api/v1/root.rb
+++ app/apis/api/v1/root.rb
@@ -4,7 +4,35 @@ module API
       version 'v1', using: :path
       format :json
 
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        error!({ error: 'Not Found', detail: "#{e.message}" }, 404)
+      end
+
+      rescue_from Grape::Exceptions::ValidationErrors do |e|
+        error!({ error: 'Community id is invalid',
+                 detail: "#{e.message}" },
+               400)
+      end
+
+      rescue_from :all do |e|
+        error!({ error: 'Internal server error',
+                 detail: "#{e.message}" },
+               500)
+      end
+
       mount API::V1::Communities
+
+      route :any, '*path' do
+        error!({ error:  'Route Not Found',
+                 detail: "No such route '#{request.path}'"},
+               500)
+      end
+
+      route :any do
+        error!({ error:  'Route Not Found',
+                 detail: "No such route '#{request.path}'"},
+               500)
+      end
     end
   end
 end
