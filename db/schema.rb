b8e71cc643eede8fb5915059dd0ddae0eccc3c5a
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-06-20 16:21:05 +0900

    migrate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504194346) do
+ActiveRecord::Schema.define(version: 20170620070735) do
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string   "access_token", null: false
+    t.integer  "user_id",      null: false
+    t.datetime "expires_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["access_token"], name: "index_api_keys_on_access_token", unique: true
+    t.index ["user_id"], name: "index_api_keys_on_user_id"
+  end
 
   create_table "communities", force: :cascade do |t|
     t.string   "name",             null: false
