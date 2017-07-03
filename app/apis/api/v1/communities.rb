31813e905f74d06584e27df0c620ec51acfa9968
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-07-03 17:41:18 +0900

    authenticate!を付与

--- app/apis/api/v1/communities.rb
+++ app/apis/api/v1/communities.rb
@@ -5,10 +5,6 @@ module API
       formatter :json, Grape::Formatter::Jbuilder
 
       resource :communities do
-        before do
-          authenticate!
-        end
-
         desc 'コミュニティ詳細取得'
         params do
           requires :id, type: Integer
@@ -17,6 +13,7 @@ module API
         end
 
         get '/:id', jbuilder: 'v1/community' do
+          authenticate!
           @community = Community.find(params[:id])
           @past_cyphers = @community.cyphers.
                                       where('cypher_from < ?', Date.today.to_datetime).
@@ -26,35 +23,43 @@ module API
                                       order(:cypher_from).all
         end
 
-
-        get '/my_communities/:id', jbuilder: 'v1/community_summary' do
-          @communities = @current_user.
-                              participating_communities.
-                              joins(:community_participants).
-                              includes(:community_participants).
-                              order("community_participants.created_at DESC").
-                              page(params[:since_id]).
-                              per(params[:limit])
+        resource :my_communities do
+          get '/my_communities/:id', jbuilder: 'v1/community_summary' do
+            authenticate!
+            @communities = @current_user.
+                                participating_communities.
+                                joins(:community_participants).
+                                includes(:community_participants).
+                                order("community_participants.created_at DESC").
+                                page(params[:since_id]).
+                                per(params[:limit])
+          end
         end
 
-        get '/hosting_communities/:id', jbuilder: 'v1/community_summary' do
-          @communities = @current_user.
-                              hosting_communities.
-                              joins(:community_hosts).
-                              includes(:community_hosts).
-                              order("community_hosts.created_at DESC").
-                              page(params[:since_id]).
-                              per(params[:limit])
+        resource :hosting_communities do
+          get '/hosting_communities/:id', jbuilder: 'v1/community_summary' do
+            authenticate!
+            @communities = @current_user.
+                                hosting_communities.
+                                joins(:community_hosts).
+                                includes(:community_hosts).
+                                order("community_hosts.created_at DESC").
+                                page(params[:since_id]).
+                                per(params[:limit])
+          end
         end
 
-        get '/following_communities/:id', jbuilder: 'v1/community_summary' do
-          @communities = @current_user.
-                              following_communities.
-                              joins(:community_followers).
-                              includes(:community_followers).
-                              order("community_followers.created_at DESC").
-                              page(params[:since_id]).
-                              per(params[:limit])
+        resource :following_communities do
+          get '/following_communities/:id', jbuilder: 'v1/community_summary' do
+            authenticate!
+            @communities = @current_user.
+                                following_communities.
+                                joins(:community_followers).
+                                includes(:community_followers).
+                                order("community_followers.created_at DESC").
+                                page(params[:since_id]).
+                                per(params[:limit])
+          end
         end
       end
 
