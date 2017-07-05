809b95fc0942f28fe867843c8f8861850bccbf69
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-07-06 01:11:34 +0900

    communities一覧のコントローラ変更

--- app/apis/api/v1/communities.rb
+++ app/apis/api/v1/communities.rb
@@ -8,8 +8,6 @@ module API
         desc 'コミュニティ詳細取得'
         params do
           requires :id, type: Integer
-          optional :since_id, type: Integer
-          optional :limit, type: Integer
         end
 
         get '/:id', jbuilder: 'v1/community' do
@@ -22,47 +20,63 @@ module API
                                       where('cypher_from >= ?', Date.today.to_datetime).
                                       order(:cypher_from).all
         end
-
-        resource :my_communities do
-          get '/my_communities/:id', jbuilder: 'v1/community_summary' do
-            authenticate!
-            @communities = @current_user.
-                                participating_communities.
-                                joins(:community_participants).
-                                includes(:community_participants).
-                                order("community_participants.created_at DESC").
-                                page(params[:since_id]).
-                                per(params[:limit])
-          end
+      end
+      resource :my_communities do
+        params do
+          requires :page, type: Integer
         end
 
-        resource :hosting_communities do
-          get '/hosting_communities/:id', jbuilder: 'v1/community_summary' do
-            authenticate!
-            @communities = @current_user.
-                                hosting_communities.
-                                joins(:community_hosts).
-                                includes(:community_hosts).
-                                order("community_hosts.created_at DESC").
-                                page(params[:since_id]).
-                                per(params[:limit])
-          end
-        end
-
-        resource :following_communities do
-          get '/following_communities/:id', jbuilder: 'v1/community_summary' do
-            authenticate!
-            @communities = @current_user.
-                                following_communities.
-                                joins(:community_followers).
-                                includes(:community_followers).
-                                order("community_followers.created_at DESC").
-                                page(params[:since_id]).
-                                per(params[:limit])
-          end
+        get '/', jbuilder: 'v1/community_summary' do
+          authenticate!
+          @communities = @current_user.
+                              participating_communities.
+                              joins(:community_participants).
+                              includes(:community_participants).
+                              order("community_participants.created_at DESC").
+                              page(params[:page])
+          @communities_total = @current_user.
+                                  participating_communities.count
+          @current_page = params[:page]
         end
       end
 
+      resource :hosting_communities do
+        params do
+          requires :page, type: Integer
+        end
+
+        get '/', jbuilder: 'v1/community_summary' do
+          authenticate!
+          @communities = @current_user.
+                              hosting_communities.
+                              joins(:community_hosts).
+                              includes(:community_hosts).
+                              order("community_hosts.created_at DESC").
+                              page(params[:page])
+          @communities_total = @current_user.
+                                    hosting_communities.count
+          @current_page = params[:page]
+        end
+      end
+
+      resource :following_communities do
+        params do
+          requires :page, type: Integer
+        end
+
+        get '/', jbuilder: 'v1/community_summary' do
+          authenticate!
+          @communities = @current_user.
+                              following_communities.
+                              joins(:community_followers).
+                              includes(:community_followers).
+                              order("community_followers.created_at DESC").
+                              page(params[:page])
+          @communities_total = @current_user.
+                                  following_communities.count
+          @current_page = params[:page]
+        end
+      end
     end
   end
 end
