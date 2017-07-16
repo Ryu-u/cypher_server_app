7449a0dc347b5eafadc4b407439460984008c473
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-07-17 00:33:12 +0900

    サイファーファクトリ修正

--- spec/factories/cyphers.rb
+++ spec/factories/cyphers.rb
@@ -8,9 +8,19 @@ FactoryGirl.define do
     name {Faker::Team.name}
     serial_num 1
     info {Faker::ChuckNorris.fact}
-    cypher_from Date.today.to_datetime
-    cypher_to Date.today.to_datetime
+    cypher_from DateTime.now + 1
+    cypher_to DateTime.now + 2
     place {Faker::Address.city}
     capacity 10
+
+    trait :with_tag do
+      after(:create) do |cypher|
+        3.times do
+          create(:cypher_tag,
+                 cypher: cypher,
+                 tag: create(:tag))
+        end
+      end
+    end
   end
 end
