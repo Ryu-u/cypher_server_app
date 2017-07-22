b46319c4bc70bb58d61f15c55fd929807b3c65b5
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-07-23 01:19:00 +0900

    endズレ修正

--- app/apis/api/v1/cyphers.rb
+++ app/apis/api/v1/cyphers.rb
@@ -83,64 +83,57 @@ module API
             status :conflict
           end
         end
+      end
 
-        desc '参加サイファー一覧表示'
-        resource :participating_cyphers do
-          params do
-            requires :since_id,     type: Integer
-            requires :cypher_from,  type: DateTime
-          end
-
-          get '/', jbuilder: 'v1/cypher_summary' do
-            authenticate!
-            if params[:cypher_from].present?
-              cypher_from = params[:cypher_from]
-            else
-              cypher_from = DateTime.now
-            end
-
-            all_cyphers = @current_user.
-                participating_cyphers.
-                includes(:community).
-                where.has{|cypher| (cypher.cypher_from > cypher_from) |
-                (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
-
-            @cyphers = all_cyphers.
-                ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
-                limit(20)
-            @cyphers_total = all_cyphers.count
-
-          end
+      desc '参加サイファー一覧表示'
+      resource :participating_cyphers do
+        params do
+          requires :since_id,     type: Integer
+          requires :cypher_from,  type: DateTime
         end
-
-        desc 'ホスト中のサイファー表示'
-        resource :hosting_cyphers do
-          params do
-            requires :since_id,     type: Integer
-            requires :cypher_from,  type: DateTime
+       get '/', jbuilder: 'v1/cypher_summary' do
+          authenticate!
+          if params[:cypher_from].present?
+            cypher_from = params[:cypher_from]
+          else
+            cypher_from = DateTime.now
           end
-
-          get '/', jbuilder: 'v1/cypher_summary' do
-            authenticate!
-            if params[:cypher_from].present?
-              cypher_from = params[:cypher_from]
-            else
-              cypher_from = DateTime.now
-            end
-
-            all_cyphers = @current_user.
-                hosting_cyphers.
-                includes(:community).
-                where.has{|cypher| (cypher.cypher_from > cypher_from) |
-                (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
-
-            @cyphers = all_cyphers.
-                ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
-                limit(20)
-            @cyphers_total = all_cyphers.count
+         all_cyphers = @current_user.
+              participating_cyphers.
+              includes(:community).
+              where.has{|cypher| (cypher.cypher_from > cypher_from) |
+              (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
+         @cyphers = all_cyphers.
+              ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
+              limit(20)
+          @cyphers_total = all_cyphers.count
+       end
+      end
+     desc 'ホスト中のサイファー表示'
+      resource :hosting_cyphers do
+        params do
+          requires :since_id,     type: Integer
+          requires :cypher_from,  type: DateTime
+        end
+       get '/', jbuilder: 'v1/cypher_summary' do
+          authenticate!
+          if params[:cypher_from].present?
+            cypher_from = params[:cypher_from]
+          else
+            cypher_from = DateTime.now
           end
+         all_cyphers = @current_user.
+              hosting_cyphers.
+              includes(:community).
+              where.has{|cypher| (cypher.cypher_from > cypher_from) |
+              (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
+         @cyphers = all_cyphers.
+              ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
+              limit(20)
+          @cyphers_total = all_cyphers.count
         end
       end
     end
   end
 end
+
