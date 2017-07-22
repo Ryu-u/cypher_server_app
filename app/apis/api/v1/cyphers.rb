a8aa0a8ce11de931cb67cf40393240e101de033b
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-07-23 01:05:13 +0900

    サイファーcrud作成&ソース整えた

--- app/apis/api/v1/cyphers.rb
+++ app/apis/api/v1/cyphers.rb
@@ -4,7 +4,9 @@ module API
       format :json
       formatter :json, Grape::Formatter::Jbuilder
 
+
       resource :cyphers do
+        desc 'サイファー一覧表示'
         params do
           requires :since_id,     type: Integer
           requires :cypher_from,  type: DateTime
@@ -18,18 +20,19 @@ module API
             cypher_from = DateTime.now
           end
 
+          # kaminariじゃなくて、サイファー開始時刻とidでページネーションを実装
           all_cyphers = Cypher.
-                            includes(:community).
-                            where.has{|cypher| (cypher.cypher_from > cypher_from) |
-                            (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
-
+              includes(:community).
+              where.has{|cypher| (cypher.cypher_from > cypher_from) |
+              (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
 
           @cyphers = all_cyphers.
-                              ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
-                              limit(20)
+              ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
+              limit(20)
           @cyphers_total = Cypher.where.has{|cypher| (cypher.cypher_from > DateTime.now)}.count
         end
 
+        desc 'サイファー詳細取得'
         params do
           requires :id, type: Integer
         end
@@ -39,74 +42,105 @@ module API
           @cypher = Cypher.find(params[:id])
         end
 
+        desc 'サイファー情報更新'
         params do
-          requires :since_id,     type: Integer
+          requires :id,           type: Integer
+          requires :name,         type: String
+          requires :info,         type: String
           requires :cypher_from,  type: DateTime
+          requires :cypher_to,    type: DateTime
+          requires :place,        type: String
+          optional :capacity,     type: Integer
         end
-        get '/:id/posts' do
-          # TODO 実装待ち
-        end
-      end
-
-      resource :participating_cyphers do
-        params do
-          requires :since_id,     type: Integer
-          requires :cypher_from,  type: DateTime
-        end
-
-        get '/', jbuilder: 'v1/cypher_summary' do
+        put '/:id' do
           authenticate!
-          if params[:cypher_from].present?
-            cypher_from = params[:cypher_from]
+          @cypher = Cypher.find(params[:id])
+          if @current_user.hosting_cypher?(@cypher.id)
+            cypher = @cypher.update_attributes(
+                name:         params[:name],
+                info:         params[:info],
+                cypher_from:  params[:cypher_from],
+                cypher_to:    params[:cypher_to],
+                place:        params[:place],
+                capacity:     params[:capacity]
+            )
           else
-            cypher_from = DateTime.now
+            status :conflict
+          end
+        end
+
+        desc 'サイファー削除'
+        params do
+          requires :id,  type: Integer
+        end
+
+        delete '/:id' do
+          authenticate!
+          @cypher = Cypher.find(params[:id])
+          if @current_user.hosting_cypher?(@cypher.id)
+            @cypher.destroy!
+          else
+            status :conflict
+          end
+        end
+
+        desc '参加サイファー一覧表示'
+        resource :participating_cyphers do
+          params do
+            requires :since_id,     type: Integer
+            requires :cypher_from,  type: DateTime
           end
 
-          all_cyphers = @current_user.
-                          participating_cyphers.
-                          includes(:community).
-                          where.has{|cypher| (cypher.cypher_from > cypher_from) |
-                                    (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
+          get '/', jbuilder: 'v1/cypher_summary' do
+            authenticate!
+            if params[:cypher_from].present?
+              cypher_from = params[:cypher_from]
+            else
+              cypher_from = DateTime.now
+            end
 
+            all_cyphers = @current_user.
+                participating_cyphers.
+                includes(:community).
+                where.has{|cypher| (cypher.cypher_from > cypher_from) |
+                (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
 
-          @cyphers = all_cyphers.
-                          ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
-                          limit(20)
-          @cyphers_total = all_cyphers.count
+            @cyphers = all_cyphers.
+                ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
+                limit(20)
+            @cyphers_total = all_cyphers.count
 
+          end
         end
-      end
 
-      resource :hosting_cyphers do
-        params do
-          requires :since_id,     type: Integer
-          requires :cypher_from,  type: DateTime
-        end
-
-        get '/', jbuilder: 'v1/cypher_summary' do
-          authenticate!
-          if params[:cypher_from].present?
-            cypher_from = params[:cypher_from]
-          else
-            cypher_from = DateTime.now
+        desc 'ホスト中のサイファー表示'
+        resource :hosting_cyphers do
+          params do
+            requires :since_id,     type: Integer
+            requires :cypher_from,  type: DateTime
           end
 
-          all_cyphers = @current_user.
-                            hosting_cyphers.
-                            includes(:community).
-                            where.has{|cypher| (cypher.cypher_from > cypher_from) |
-                                                (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
+          get '/', jbuilder: 'v1/cypher_summary' do
+            authenticate!
+            if params[:cypher_from].present?
+              cypher_from = params[:cypher_from]
+            else
+              cypher_from = DateTime.now
+            end
 
+            all_cyphers = @current_user.
+                hosting_cyphers.
+                includes(:community).
+                where.has{|cypher| (cypher.cypher_from > cypher_from) |
+                (cypher.id > params[:since_id]) & (cypher.cypher_from == cypher_from)}
 
-          @cyphers = all_cyphers.
-                          ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
-                          limit(20)
-          @cyphers_total = all_cyphers.count
-
+            @cyphers = all_cyphers.
+                ordering{|cypher| [cypher.cypher_from.asc, cypher.id.asc]}.
+                limit(20)
+            @cyphers_total = all_cyphers.count
+          end
         end
       end
-
-
     end
   end
 end
