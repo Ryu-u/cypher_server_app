01dd8317c0873281df216160cf74a112de6839cf
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-08-29 05:52:29 +0900

    コミュニティルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   mount API::Root => '/api'
+
+  resources :communities, format: false
+  resources :communities, as: :my_communities,
+            format: false,
+            only: [:index]
+  resources :communities, as: :hosting_communities,
+            format: false,
+            only: [:index]
+  resources :communities, as: :following_communities,
+            format: false,
+            only: [:index]
 end
