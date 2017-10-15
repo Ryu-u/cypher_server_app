a47775c61ab45729ab88c50f63018ac280d01a59
anekawa <po1nt0fdepartur1ng@gmail.com>
2017-10-15 19:29:55 +0900

    サイファーのホストかどうか確認

--- app/controllers/cyphers_controller.rb
+++ app/controllers/cyphers_controller.rb
@@ -18,59 +18,74 @@ class CyphersController < ApplicationController
 
   def new
     authenticate!
-    #TODO コミュニティのホストかどうか
-    community_name = Community.find(params[:id]).name
-    @cypher = Cypher.new(name: community_name)
+    @community = Community.find(params[:id])
+    if @current_user.hosting_community?(@community.id)
+      @cypher = Cypher.new(name: @community.name)
+    else
+      redirect_to @community
+    end
   end
 
   def create
     authenticate!
-    #TODO コミュニティのホストかどうか
-    params = cypher_params
-    community = Community.find(params[:id])
-    @cypher = Cypher.new(name: params[:name],
-                         place: params[:place],
-                         info: params[:ifno],
-                         cypher_from: params[:cypher_from],
-                         cypher_to: params[:cypher_to])
-    community.cyphers << @cypher
-    @cypher.host = @current_user
-    if @cypher.save!
-      redirect_to @cypher
+    @community = Community.find(params[:id])
+    if @current_user.hosting_community?(@community.id)
+      params = cypher_params
+      community = Community.find(params[:id])
+      @cypher = Cypher.new(name: params[:name],
+                           place: params[:place],
+                           info: params[:ifno],
+                           cypher_from: params[:cypher_from],
+                           cypher_to: params[:cypher_to])
+      community.cyphers << @cypher
+      @cypher.host = @current_user
+      if @cypher.save!
+        redirect_to @cypher
+      else
+        render 'new'
+      end
     else
-      render 'new'
+      redirect_to @community
     end
   end
 
   def edit
     authenticate!
-    #TODO サイファーのホストかどうか
     @cypher = Cypher.find(params[:id])
+    if @current_user.hosting_cypher?(params[:id])
+      @cypher
+    else
+      redirect_to @cypher
+    end
   end
 
   def update
     authenticate!
-    #TODO サイファーのホストかどうか
-    params = cypher_params
-    @cypher.name = params[:name]
-    @cypher.place = params[:place]
-    @cypher.info = params[:ifno]
-    @cypher.cypher_from = params[:cypher_from]
-    @cypher.cypher_to = params[:cypher_to]
-    if @cypher.save!
-      redirect_to @cypher
+    @cypher = Cypher.find(params[:id])
+    if @current_user.hosting_cypher?(@cypher.id)
+      params = cypher_params
+      @cypher.name = params[:name]
+      @cypher.place = params[:place]
+      @cypher.info = params[:ifno]
+      @cypher.cypher_from = params[:cypher_from]
+      @cypher.cypher_to = params[:cypher_to]
+      if @cypher.save!
+        redirect_to @cypher
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect_to @cypher
     end
-
   end
 
   def destroy
     authenticate!
-    #TODO サイファーのホストかどうか
     @cypher = Cypher.find(params[:id])
+    if @current_user.hosting_cypher?(@cypher.id)
     @cypher.destroy!
     # render 成功ページへ
+    end
   end
 
   def my_cyphers
